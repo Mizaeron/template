d5494a5f17c300e15662943ec3645f397808e587
Miza <mizaeron@gmail.com>
2025-12-20 14:25:43 +0000

    commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,6 @@
 import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
-import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 export default defineConfig([
   {
